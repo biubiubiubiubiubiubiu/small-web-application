8cf726ad36e8c818a90eac7b94ceb07567d887b3
ruofan <rshen3@jhu.edu>
2018-02-03 00:10:15 -0500

    Backend and Frontend raw construction

--- NeteaseDemo/NeteaseDemo/src/main/webapp/static/js/app.js
+++ NeteaseDemo/NeteaseDemo/src/main/webapp/static/js/app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-var App = angular.module('myApp',[]);
+var App = angular.module('myApp',[
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+]);
+
+App.config(function ($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: '/static/template/main.html',
+        controller: 'UserController'
+    })
+});
